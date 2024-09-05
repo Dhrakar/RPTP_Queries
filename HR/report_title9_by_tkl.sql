09d590440fec1df787f412ee324599a011154245
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2024-09-05 08:35:30 -0800

    adj to better report completions

--- HR/report_title9_by_tkl.sql
+++ HR/report_title9_by_tkl.sql
@@ -5,15 +5,42 @@
 -- a supplied TKL.  Shows NULL if a person has never done Title IX. 
 -- ==========================================================================
 SELECT
-  emp.spriden_id             AS uaid,
-  emp.spriden_first_name     AS first_name,
-  emp.spriden_last_name      AS last_name,
-  emp.pebempl_orgn_code_dist AS tkl,
-  typ.ptrcert_desc           AS certification,
+  emp.spriden_id                AS uaid,
+  emp.spriden_first_name        AS first_name,
+  emp.spriden_last_name         AS last_name,
+  to_char(
+    emp.pebempl_current_hire_date,
+    'mm/dd/yyyy'
+  )                             AS hired_on,
+  to_char(
+    emp.nbrbjob_begin_date,
+    'mm/dd/yyyy'
+  )                             AS position_start,
+  to_char(
+    emp.nbrbjob_end_date,
+    'mm/dd/yyyy'
+  )                             AS position_end,
+  emp.nbrjobs_ecls_code
+    || ':' || (
+      SELECT a.ptrecls_long_desc 
+      FROM PAYROLL.PTRECLS a 
+      WHERE a.ptrecls_code = emp.nbrjobs_ecls_code 
+    )                           AS employee_class,
+  -- emp.pebempl_orgn_code_dist    AS tkl,
+  -- org.level1                    AS campus,
+  org.title2                    AS cabinet,
+  org.title                     AS title,
+  typ.ptrcert_desc              AS certification,
   to_char(
     crt.pprcert_cert_date,
     'mm/dd/yyyy'
-  )                          AS certification_date
+  )                             AS certification_date,
+  CASE 
+    WHEN to_char( crt.pprcert_cert_date, 'mm/dd/yyyy' ) = '06/20/2024' AND emp.pebempl_current_hire_date < to_date ('11/01/2023', 'mm/dd/yyyy') THEN 'ERROR'
+    WHEN SYSDATE - crt.pprcert_cert_date > 365 THEN 'EXPIRED'
+    WHEN typ.ptrcert_desc IS NULL THEN 'NOT DONE'
+    ELSE 'OK'
+  END    AS status
 FROM
   -- get the currently active employees
   REPORTS.N_ACTIVE_JOBS emp
@@ -29,9 +56,50 @@ FROM
   LEFT OUTER JOIN PAYROLL.PTRCERT typ ON (
     typ.ptrcert_code = crt.pprcert_cert_code
   )
+  -- get this person's home dept and hierarchy
+  LEFT OUTER JOIN REPORTS.FTVORGN_LEVELS org ON (
+    org.orgn_code = emp.pebempl_orgn_code_home 
+  )
 WHERE
   emp.nbrbjob_contract_type = 'P' -- only grab primary positions
-  AND emp.pebempl_orgn_code_dist = :tkl -- filter for TKL
+  -- do not include student employees or the old Adjunct categories
+  AND emp.nbrjobs_ecls_code IN (
+--    'A9', --'Faculty',
+--    'AR', --'Faculty',
+    'EX', --'Officers/Sr. Administrators',
+    'F9', --'Faculty',
+    'FN', --'Faculty',
+    'FR', --'Officers/Sr. Administrators',
+    'FT', --'Adjunct Faculty',
+    'FW', --'Adjunct Faculty',
+    'CR', --'Staff',
+    'CT', --'Staff',
+    'NR', --'Staff',
+    'NT', --'Staff',
+    'NX', --'Staff',
+    'XR', --'Staff',
+    'XT', --'Staff',
+    'XX', --'Staff',
+--    'GN', --'Student',
+--    'GT', --'Student',
+--    'SN', --'Student',
+--    'ST' --'Student',
+    '00' -- dummy value to keep from futzing with the trailing ','
+  )
+  -- uncomment to get a specific TKL
+  -- AND emp.pebempl_orgn_code_dist = :tkl -- filter for TKL
+  -- ----------------------------------
+--    -- just UAF TKLs
+--  AND emp.pebempl_orgn_code_dist IN (
+--    SELECT
+--      ntr2tkl_orgn_code
+--    FROM
+--      POSNCTL.NTR2TKL
+--    WHERE 
+--      ntr2tkl_mau_code = 'F'
+--  )
+    -- just folks that roll up to UAFTOT
+  AND org.level1 = 'UAFTOT'
   AND (
     -- check for null in case this person does not have any T9 certs
     -- otherwise, find the most recent
@@ -40,7 +108,10 @@ WHERE
       SELECT max(crt2.pprcert_cert_date)
       FROM PAYROLL.PPRCERT crt2
       WHERE crt2.pprcert_pidm = crt.pprcert_pidm
-        AND crt2.pprcert_cert_code = crt.pprcert_cert_code
+        -- AND crt2.pprcert_cert_code = crt.pprcert_cert_code
     )
   )
+ORDER BY
+  org.title2, org.title,
+  emp.spriden_id
 ;
