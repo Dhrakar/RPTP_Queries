c438a0fcc5d6572a2785203b3fc320b9b743f070
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2025-08-28 14:09:28 -0800

    Lots of updates to adapt for UAF - add pre-banner to cumulative time - rearrange teh cols

--- HR/UA Cumulative Service Longevity.sql
+++ HR/UA Cumulative Service Longevity.sql
@@ -32,7 +32,7 @@ with assignments as (
                                                            and job.nbrbjob_suff = pos2.nbrjobs_suff
                                                            and pos2.nbrjobs_effective_date <= to_date('31-DEC-' || :Calendar_Year_YYYY, 'DD-MON-YYYY'))
      where pos.nbrjobs_ecls_code in ('NR', 'XR', 'CR', 'EX', 'FR', 'F9', 'FN', 'AR', 'A9') -- Historical Employee Classifications for Cumulative Service
-       and nbrbjob_pidm =
+       -- and nbrbjob_pidm =
        and nbrbjob_contract_type in ('P', 'S') -- Types of contracts considered.
 
 ), group_begin as (
@@ -104,26 +104,38 @@ group by pidm)
               || ', ' || a.spraddr_stat_code 
               || ', ' || a.spraddr_zip "Mailing Address",
            emp.nbrjobs_ecls_code "ECLS",
+           boss.spriden_id "Supervisor UA ID",
+           boss.spriden_last_name || ', ' || 
+           boss.spriden_first_name || ' ' || 
+           substr(boss.spriden_mi,0,1) "Supervisor Name",
+           busr.gobtpac_external_user || '@alaska.edu' "Supervisor Email",
            emp.pebempl_first_hire_date "Original Hire Date",
            emp.pebempl_adj_service_date "Adjusted Service Date",
            emp.nbrbjob_begin_date "Curr. Position Start Date",
            emp.nbrbjob_end_date "Curr. Position End Date",
-          --  boss.spriden_id "Supervisor UA ID",
-          --  boss.spriden_last_name || ', ' || 
-          --  boss.spriden_first_name || ' ' || 
-          --  substr(boss.spriden_mi,0,1) "Supervisor Name",
-          --  busr.gobtpac_external_user || '@alaska.edu' "Supervisor Email",
-           service.yos "Cumulative Service Years",
-           case
-             when trunc(service.yos) != 0
-              and remainder(trunc(service.yos), 5) = 0 then 'Y'
-             else 'N'
-           end "Award Year",
-           round(((to_date('31-DEC-' || :Calendar_Year_YYYY, 'DD-MON-YYYY') - emp.pebempl_adj_service_date) / 365.25), 1) "Adj. Service Years",
+           -- get the total years of service using the adj service date
+           floor((to_date('31-DEC-' || :Calendar_Year_YYYY, 'DD-MON-YYYY') - emp.pebempl_adj_service_date) / 365.25) "Adj. Service Years",
            CASE -- set the flag for folks hired prior to 1996
              WHEN to_date('01/01/1996', 'mm/dd/yyyy') > emp.pebempl_first_hire_date THEN 'Y'
              ELSE 'N'
-           END "Pre-Banner"
+           END "Pre-Banner",
+           CASE -- if the person has pre-banner time, add it to the cumulative years.  Take the floor to show the highest year.
+             WHEN to_date('01/01/1996', 'mm/dd/yyyy') > emp.pebempl_first_hire_date THEN floor((( to_date('01/01/1996', 'mm/dd/yyyy') - emp.pebempl_first_hire_date ) / 365.25 ) + service.yos)
+             ELSE floor(service.yos) 
+           END "Cumulative Years",
+           CASE -- see if the cumulaticve total falls ona 5 year boundary
+             WHEN
+               CASE
+                 WHEN to_date('01/01/1996', 'mm/dd/yyyy') > emp.pebempl_first_hire_date THEN floor((( to_date('01/01/1996', 'mm/dd/yyyy') - emp.pebempl_first_hire_date ) / 365.25 ) + service.yos)
+                 ELSE floor(service.yos) 
+               END > 0
+               AND remainder (
+                 CASE
+                   WHEN to_date('01/01/1996', 'mm/dd/yyyy') > emp.pebempl_first_hire_date THEN floor((( to_date('01/01/1996', 'mm/dd/yyyy') - emp.pebempl_first_hire_date ) / 365.25 ) + service.yos)
+                   ELSE floor(service.yos) 
+                 END, 5) = 0 THEN 'Y'
+              ELSE 'N'
+            END "Milestone"
       from n_active_jobs emp
       join spbpers bio on bio.spbpers_pidm = emp.pebempl_pidm
       join gobtpac usr on usr.gobtpac_pidm = emp.pebempl_pidm
@@ -131,13 +143,13 @@ group by pidm)
       left join ftvorgn_levels_titles org on org.orgn_code = emp.pebempl_orgn_code_home
       left join spraddr a on a.spraddr_pidm = emp.pebempl_pidm
                          and a.spraddr_atyp_code = 'MA'
-      -- left join ner2sup sup on emp.nbrbjob_pidm = sup.ner2sup_pidm
-      --                      and emp.nbrbjob_posn = sup.ner2sup_posn
-      --                      and emp.nbrbjob_suff = sup.ner2sup_suff
-      --                      and sup.ner2sup_sup_ind = 'Y'
-      -- left join spriden boss on boss.spriden_pidm = sup.ner2sup_sup_pidm
-                            -- and boss.spriden_change_ind is null
---       left join gobtpac busr on sup.ner2sup_sup_pidm = busr.gobtpac_pidm
+      left join ner2sup sup on emp.nbrbjob_pidm = sup.ner2sup_pidm
+                           and emp.nbrbjob_posn = sup.ner2sup_posn
+                           and emp.nbrbjob_suff = sup.ner2sup_suff
+                           and sup.ner2sup_sup_ind = 'Y'
+      left join spriden boss on boss.spriden_pidm = sup.ner2sup_sup_pidm
+                            and boss.spriden_change_ind is null
+      left join gobtpac busr on sup.ner2sup_sup_pidm = busr.gobtpac_pidm
      where emp.nbrbjob_contract_type = 'P'
        and emp.nbrjobs_ecls_code in ('EX', 'NR', 'XR', 'CR')
        and (a.spraddr_seqno is null
@@ -145,12 +157,12 @@ group by pidm)
                                  from spraddr a2
                                 where a.spraddr_pidm = a2.spraddr_pidm
                                   and a.spraddr_atyp_code = a2.spraddr_atyp_code))
---       and (sup.ner2sup_effective_date is null
---         or sup.ner2sup_effective_date = (select max(sup2.ner2sup_effective_date)
---                                           from ner2sup sup2
---                                          where sup.ner2sup_pidm = sup2.ner2sup_pidm
---                                            and sup.ner2sup_posn = sup2.ner2sup_posn
---                                            and sup.ner2sup_suff = sup2.ner2sup_suff
---                                            and sup2.ner2sup_effective_date <= sysdate))
+      and (sup.ner2sup_effective_date is null
+        or sup.ner2sup_effective_date = (select max(sup2.ner2sup_effective_date)
+                                          from ner2sup sup2
+                                         where sup.ner2sup_pidm = sup2.ner2sup_pidm
+                                           and sup.ner2sup_posn = sup2.ner2sup_posn
+                                           and sup.ner2sup_suff = sup2.ner2sup_suff
+                                           and sup2.ner2sup_effective_date <= sysdate))
   order by "Campus",
            service.yos desc;
